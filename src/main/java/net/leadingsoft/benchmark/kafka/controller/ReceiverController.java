4e356f5b35e2a7e61f5a8094b78c3e5013b3fadb
gakugun.cho <zhang_xuejun@leadingsoft.co.jp>
2021-01-24 18:31:32 +0900

    support json

--- src/main/java/net/leadingsoft/benchmark/kafka/controller/ReceiverController.java
+++ src/main/java/net/leadingsoft/benchmark/kafka/controller/ReceiverController.java
@@ -1,5 +1,8 @@
 package net.leadingsoft.benchmark.kafka.controller;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,7 +21,29 @@ public class ReceiverController {
   KafkaTemplate kafkaTemplate;
 
   @PostMapping("/api/{topic}")
-  public ResponseEntity<Void> receive(@PathVariable String topic, @RequestBody String body) throws ExecutionException, InterruptedException {
+  public ResponseEntity<Void> receive(@PathVariable String topic, @RequestBody String body)
+      throws ExecutionException, InterruptedException {
+    kafkaTemplate.send(topic, body).get();
+    return new ResponseEntity<>(HttpStatus.OK);
+  }
+
+  @PostMapping("/api/json/{id}/{topic}")
+  public ResponseEntity<Void> receiveJson(@PathVariable String id, @PathVariable String topic, @RequestBody String body)
+      throws ExecutionException, InterruptedException {
+    Object o = JSON.parse(body);
+    if (o instanceof JSONObject) {
+      JSONObject jo = (JSONObject) o;
+      if (!id.equals(jo.getString("appId"))) {
+        throw new IllegalArgumentException("illegal id.");
+      }
+    } else if (o instanceof JSONArray) {
+      ((JSONArray) o).forEach(jo -> {
+        if (!id.equals(((JSONObject) jo).getString("appId"))) {
+          throw new IllegalArgumentException("illegal id.");
+        }
+      });
+    }
+
     kafkaTemplate.send(topic, body).get();
     return new ResponseEntity<>(HttpStatus.OK);
   }
